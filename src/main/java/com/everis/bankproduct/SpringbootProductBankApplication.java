c177ab3a441580444620a50195bcbcb5a7e5a996
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-09 16:31:54 -0500

    feat: Added config -server

--- src/main/java/com/everis/bankproduct/SpringbootProductBankApplication.java
+++ src/main/java/com/everis/bankproduct/SpringbootProductBankApplication.java
@@ -1,67 +1,17 @@
 package com.everis.bankproduct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-
-import com.everis.bankproduct.domain.ProductBank;
-import com.everis.bankproduct.domain.TypeProductBank;
-import com.everis.bankproduct.repository.ProductBankRepository;
-import com.everis.bankproduct.repository.TypeProductRepository;
-
-import reactor.core.publisher.Flux;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringbootProductBankApplication {
-//implements CommandLineRunner  {
-
-	
-	@Autowired
-	private ProductBankRepository productbank;
-
-	@Autowired
-	private TypeProductRepository typeprodbank;
-	
-	@Autowired
-	private ReactiveMongoTemplate template;
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootProductBankApplication.class, args);
 	}
 
-//	@Override
-//	public void run(String... args) throws Exception {
-//		template.dropCollection(ProductBank.class).subscribe();
-//		template.dropCollection(TypeProductBank.class).subscribe();
-//	
-//		TypeProductBank type1 = new TypeProductBank();
-//		type1.setDescription("Cuenta Bancaria");
-//				
-//		Flux.just(type1)
-//		.flatMap(tipP -> typeprodbank.save(tipP))
-//		.thenMany(	
-//				Flux.just(new ProductBank("Cuenta de ahorro" ,  type1,1),
-//						  new ProductBank("Cuentas corriente" ,  type1,2),
-//						  new ProductBank("Cuentas a plazo fijo 1" ,  type1,3),
-//						  new ProductBank("Cuenta de ahorro Personal 2 " ,  type1,4),
-//						  new ProductBank("Cuenta Corriente Personal 3 " ,  type1,5),
-//						  new ProductBank("Cuenta corriente Empresarial 2" ,  type1,6),
-//						  new ProductBank("Cuenta corriente empresarial 1" ,  type1,7),						  
-//						  new ProductBank("Cuentas a plazo fijo 2" ,  type1,8)
-//					)	
-//				).flatMap(pro -> productbank.save(pro))
-//		.subscribe(res -> System.out.println("Producto".concat(res.toString().concat(" " + "Registrado"))));
-//		
-//	}
-//		
-		
-		
-	}
+}
 
 
--- src/test/java/com/cavetech/springboot/productbank/app/SpringbootProductBankApplicationTests.java
+++ src/test/java/com/cavetech/springboot/productbank/app/SpringbootProductBankApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cavetech.springboot.productbank.app;
 
+import com.everis.bankproduct.service.impl.ProductBankServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -23,54 +24,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringbootProductBankApplicationTests {
 
-	
-	
-	@Autowired
-	private WebTestClient client;
-	
-	@Autowired
-	private ProductBankService bankService;
-	
-	
 	@Test
-	void FindAllProductBankTest() {
-		client.get()
-		.uri("/productbank/list")
-		.exchange()
-		.expectStatus().isOk()
-		.expectBodyList(ProductBank.class)
-		.consumeWith(response -> {
-			List<ProductBank> productBank = response.getResponseBody();
-			productBank.forEach(b ->{
-				System.out.println(b.getDescription());
-			});
-			
-			Assertions.assertThat(productBank.size()>0).isTrue();
-		});
+	void contextLoads() {
 	}
-	
-	@Test
-	void saveProductBank() {
-		ProductBank prod = new ProductBank("cuenta prueba2", new TypeProductBank("5e334b9f0cdb675a8671ad15","Cuenta Bancaria"),5);
-		client.post()
-		.uri("/productbank")
-		.body(Mono.just(prod), ProductBank.class)
-		.exchange()
-		.expectStatus().isOk()
-		.expectBody(ProductBank.class)
-		.consumeWith(response -> {
-			ProductBank b = response.getResponseBody();
-			System.out.println("[PRODUCTO BANCARIO REGISTRADO] " + prod);
-			Assertions.assertThat(b.getCodigo()).isNotNull().isEqualTo(5);
-			Assertions.assertThat(b.getDescription()).isNotEmpty().isEqualTo("cuenta prueba2");
-			
-		});
-	}
-	
-	
-	
 
 }
